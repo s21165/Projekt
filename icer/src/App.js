d7e44e365eff84e9d2382a4230d61ed03c7384cf
s21488-pj <s21488@pjwstk.edu.pl>
2023-05-04 18:09:40 +0200

    responsive

--- icer/src/App.js
+++ icer/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { animateScroll as scroll } from "react-scroll";
+import  logo from './data/logo.svg'
 import {
     BrowserRouter as Router,
     Routes,
@@ -17,7 +17,7 @@ import React, {useEffect, useState} from "react";
 import Logout from "./mainPages/Logout";
 
 
-function App(props) {
+function App() {
 
     const [isNavExpanded, setIsNavExpanded] = useState(false);
 
@@ -25,43 +25,47 @@ function App(props) {
 
     return (
         <Router>
-        <div className="body">
+            <div className="body">
 
-            <nav className="wholeContainer">
+                <nav className="wholeContainer">
 
-                <Routes>
+                    <Routes>
 
-                    <Route path="/" element={<Fridge/> }  />
-                    <Route path="/HistoriaZakupow" element={<History/>}/>
-                    <Route path="/Sklepy" element={<Shops/>}/>
-                    <Route path="/Konto" element={<Account/>}/>
-                    <Route path="/Powiadomienia" element={<Notifications/>}/>
-                    <Route path="/Sklepy" element={<Fridge/>}/>
-                    <Route path="/Ustawienia" element={<History/>}/>
-                    <Route path="/Pomoc" element={<Shops/>} />
-                    <Route path="/Wyloguj" element={<Logout/>} />
+                        <Route path="/" element={<Fridge/> }  />
+                        <Route path="/HistoriaZakupow" element={<History/>}/>
+                        <Route path="/Sklepy" element={<Shops/>}/>
+                        <Route path="/Konto" element={<Account/>}/>
+                        <Route path="/Powiadomienia" element={<Notifications/>}/>
+                        <Route path="/Sklepy" element={<Fridge/>}/>
+                        <Route path="/Ustawienia" element={<History/>}/>
+                        <Route path="/Pomoc" element={<Shops/>} />
+                        <Route path="/Wyloguj" element={<Logout/>} />
 
-                </Routes>
-            </nav>
-            <div>
+                    </Routes>
+                </nav>
+                <div>
 
-                <nav className ="menu">
 
-                    {isNavExpanded && (
-                        <div  className={isNavExpanded ? "visible" : "invisible"}>
+                    <nav className ="menu">
+                        <div className="logoContainer">
+                            <img src={logo} alt="Your Image" className="logo"/>
+                        </div>
 
-                            <div className="menuNav">
-                                <div className="navDiv" >
-                                    <Link to="/Sklepy"><h3>sklepy</h3></Link>
-                                </div>
-                                <div className="navDiv">
-                                    <Link to="/Ustawienia"><h3>ustawienia</h3></Link>
-                                </div>
-                                <div className="navDiv">
-                                    <Link to="/Pomoc"><h3>pomoc</h3></Link>
-                                </div>
+                        {isNavExpanded && (
+                            <div  className={isNavExpanded ? "visible" : "invisible"}>
 
-                                <div className="navDiv">
+                                <div className="menuNav">
+                                    <div className="navDiv" >
+                                        <Link to="/Sklepy"><h3>sklepy</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <Link to="/Ustawienia"><h3>ustawienia</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <Link to="/Pomoc"><h3>pomoc</h3></Link>
+                                    </div>
+
+                                    <div className="navDiv">
 
                                         <button className="menuNavExpandButton"
                                                 onClick={() => {
@@ -71,50 +75,52 @@ function App(props) {
                                         </button>
 
 
-                                </div>
+                                    </div>
 
-                                <div className="navDiv">
-                                    <Link to="/Wyloguj"><h3>wyloguj</h3></Link>
+                                    <div className="navDiv">
+                                        <Link to="/Wyloguj"><h3>wyloguj</h3></Link>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )}
-                    <div className={isNavExpanded ? "invisible" : "visible"}>
-                        <div className="menuNav">
-                            <div>
-                                <div className="navDiv" >
-                                    <Link to="/"><h3>lodówka</h3></Link>
+                        )}
+                        <div className={isNavExpanded ? "invisible" : "visible"}>
+                            <div className="menuNav">
+                                <div>
+                                    <div className="navDiv" >
+                                        <Link to="/"><h3>lodówka</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <Link to="/HistoriaZakupow"><h3>historia zakupów</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <Link to="/Sklepy"><h3>sklepy</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <Link to="/Konto"> <h3>konto</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <Link to="/Powiadomienia"> <h3>powiadomienia</h3></Link>
+                                    </div>
+                                    <div className="navDiv">
+                                        <button className="menuNavExpandButton"
+                                                onClick={() => {
+                                                    setIsNavExpanded(!isNavExpanded)
+                                                }}
+                                        ><h3>menu</h3>
+                                        </button>
+                                    </div>
+
                                 </div>
-                                <div className="navDiv">
-                                    <Link to="/HistoriaZakupow"><h3>historia zakupów</h3></Link>
-                                </div>
-                                <div className="navDiv">
-                                    <Link to="/Sklepy"><h3>sklepy</h3></Link>
-                                </div>
-                                <div className="navDiv">
-                                    <Link to="/Konto"> <h3>konto</h3></Link>
-                                </div>
-                                <div className="navDiv">
-                                    <Link to="/Powiadomienia"> <h3>powiadomienia</h3></Link>
-                                </div>
-                                <button className="menuNavExpandButton"
-                                        onClick={() => {
-                                            setIsNavExpanded(!isNavExpanded)
-                                        }}
-                                ><h3>menu</h3>
-                                </button>
 
                             </div>
-
                         </div>
-                    </div>
 
-                </nav>
+                    </nav>
 
+
+                </div>
 
             </div>
-
-        </div>
         </Router>
     )
 }
