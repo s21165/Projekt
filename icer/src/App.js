4f6b7ef70587153dbb61712924a77e9f38575cbd
s21488-pj <s21488@pjwstk.edu.pl>
2023-05-05 17:39:35 +0200

    expansiveNavButNotDone

--- icer/src/App.js
+++ icer/src/App.js
@@ -4,14 +4,14 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
-    Link, useLocation
+    Link, useLocation, Await
 } from "react-router-dom";
 import {Fridge} from "./mainPages/Fridge";
 import {History} from "./mainPages/History";
 import {Shops} from "./mainPages/Shops";
 import {Account} from "./mainPages/Account";
 import {Notifications} from "./mainPages/Notifications";
-
+import { Icon } from '@iconify/react';
 import React, {useEffect, useState} from "react";
 
 import Logout from "./mainPages/Logout";
@@ -19,13 +19,31 @@ import Logout from "./mainPages/Logout";
 
 function App() {
 
-    const [isNavExpanded, setIsNavExpanded] = useState(false);
+    const [secoundMenu, setSecoundMenu] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
+    const [isIcon, setIsIcon] = useState(true);
 
+    const handleClick = () => {
+
+        if(isOpen){
+            setIsOpen(!isOpen);
+
+            setTimeout(()=>{
+                setIsIcon(!isIcon)
+            },500)
+        }else{
+            setIsOpen(!isOpen);
+
+            setTimeout(()=>{
+                setIsIcon(!isIcon)
+            },200)
+        }
+    }
 
 
     return (
         <Router>
-            <div className="body">
+            <div className={isOpen ? "body" : "body-slimMenu"}>
 
                 <nav className="wholeContainer">
 
@@ -46,67 +64,70 @@ function App() {
                 <div>
 
 
-                    <nav className ="menu">
+                    <nav className ={isOpen ? "menu" : "slimMenu"}>
+                        <div className="navExpandButton">
+                            <button onClick={handleClick}>{ isIcon ? <Icon  icon="material-symbols:arrow-forward-ios" /> : <Icon icon="material-symbols:arrow-back-ios-new" />} </button>
+                        </div>
                         <div className="logoContainer">
-                            <img src={logo} alt="Your Image" className="logo"/>
+                            <img src={logo} alt="Your Image" className={isIcon ? "logo" : "smallLogo"}/>
                         </div>
 
-                        {isNavExpanded && (
-                            <div  className={isNavExpanded ? "visible" : "invisible"}>
+                        {secoundMenu && (
+                            <div  className={secoundMenu ? "visible" : "invisible"}>
 
                                 <div className="menuNav">
                                     <div className="navDiv" >
-                                        <Link to="/Sklepy"><h3>sklepy</h3></Link>
+                                        <Link to="/Sklepy">{ isIcon ? <h3>sklepy</h3> : <Icon className="menuIcons" icon="mdi:shop-find" />}</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <Link to="/Ustawienia"><h3>ustawienia</h3></Link>
+                                        <Link to="/Ustawienia">{isIcon ? <h3>ustawienia</h3> : <Icon className="menuIcons" icon="fa6-solid:screwdriver-wrench" />}</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <Link to="/Pomoc"><h3>pomoc</h3></Link>
+                                        <Link to="/Pomoc">{isIcon ? <h3>pomoc</h3> : <Icon className="menuIcons" icon="solar:help-linear" />}</Link>
                                     </div>
 
                                     <div className="navDiv">
 
-                                        <button className="menuNavExpandButton"
+                                        <button className="menuNavButton"
                                                 onClick={() => {
-                                                    setIsNavExpanded(!isNavExpanded)
+                                                    setSecoundMenu(!secoundMenu)
                                                 }}
-                                        ><h3>menu</h3>
+                                        >{isIcon ? <h3>menu</h3> : <Icon className="menuIcons" icon="mdi:menu-up-outline" />}
                                         </button>
 
 
                                     </div>
 
                                     <div className="navDiv">
-                                        <Link to="/Wyloguj"><h3>wyloguj</h3></Link>
+                                        <Link to="/Wyloguj">{isIcon ? <h3>wyloguj</h3> : <Icon className="menuIcons" icon="grommet-icons:logout" />}</Link>
                                     </div>
                                 </div>
                             </div>
                         )}
-                        <div className={isNavExpanded ? "invisible" : "visible"}>
+                        <div className={secoundMenu ? "invisible" : "visible"}>
                             <div className="menuNav">
                                 <div>
                                     <div className="navDiv" >
-                                        <Link to="/"><h3>lodówka</h3></Link>
+                                        <Link to="/">{isIcon ?<h3>lodówka</h3> :<Icon className="menuIcons" icon="tabler:fridge" />}</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <Link to="/HistoriaZakupow"><h3>historia zakupów</h3></Link>
+                                        <Link to="/HistoriaZakupow">{isIcon ?<h3>historia zakupów</h3> : <Icon className="menuIcons" icon="material-symbols:history-rounded" />}</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <Link to="/Sklepy"><h3>sklepy</h3></Link>
+                                        <Link to="/Sklepy">{isIcon ?<h3>sklepy</h3> : <Icon className="menuIcons" icon="mdi:shop-find" />}</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <Link to="/Konto"> <h3>konto</h3></Link>
+                                        <Link to="/Konto">{isIcon ? <h3>konto</h3> : <Icon className="menuIcons" icon="material-symbols:account-circle-outline" />}</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <Link to="/Powiadomienia"> <h3>powiadomienia</h3></Link>
+                                        <Link to="/Powiadomienia"> {isIcon ? <h3> powiadomienia</h3> : <Icon className="menuIcons" icon="ic:baseline-notifications" /> }</Link>
                                     </div>
                                     <div className="navDiv">
-                                        <button className="menuNavExpandButton"
+                                        <button className="menuNavButton"
                                                 onClick={() => {
-                                                    setIsNavExpanded(!isNavExpanded)
+                                                    setSecoundMenu(!secoundMenu)
                                                 }}
-                                        ><h3>menu</h3>
+                                        >{isIcon ?<h3>menu</h3>: <Icon className="menuIcons" icon="mdi:menu-down-outline" />}
                                         </button>
                                     </div>
 
