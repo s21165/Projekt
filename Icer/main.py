eb7ab5b56b314eee292b86f46ac9e1c0d80768bd
s21165 <56755313+s21165@users.noreply.github.com>
2023-05-16 23:28:11 +0200

    For now it's using id to delete from table, in future it can change to name of produkt

--- Icer/main.py
+++ Icer/main.py
@@ -1,4 +1,5 @@
 # Importowanie klas
+import value_manager
 from database_connector import DatabaseConnector
 from product_data import ProductData
 
@@ -18,7 +19,25 @@ product_data = ProductData(connection)
 products = product_data.fetch_products()
 
 # Wyświetlanie danych produktów
+##product_data.display_products(products)
+
+# Dodawanie produktu
+
+product_manager = value_manager.ProductManager(db_connector)  # Tworzenie instancji klasy ProductManager
+product_manager.dodaj_produkt("Kokos", 2.5, 52, 0.2, 13.8, 0.3, "Owoce", 10)
+
+
+
+# Wyświetlanie danych produktów
 product_data.display_products(products)
+
+# Odejmowanie produktu
+product_manager.odejmij_produkt(7, 1)
+
+
+# Wyświetlanie danych produktów
+##product_data.display_products(products)
+
 
 # Rozłączanie z bazą danych
 db_connector.disconnect()
---
+++ Icer/value_manager.py
@@ -0,0 +1,51 @@
+from database_connector import DatabaseConnector
+
+class ProductManager:
+    def __init__(self, database_connector):
+        self.db_connector = database_connector
+
+    def dodaj_produkt(self, nazwa, cena, kalorie, tluszcze, weglowodany, bialko, kategoria, ilosc):
+        try:
+            connection = self.db_connector.get_connection()
+            cursor = connection.cursor()
+
+            query = "INSERT INTO Produkty (nazwa, cena, kalorie, tluszcze, weglowodany, bialko, kategoria, ilosc) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            values = (nazwa, cena, kalorie, tluszcze, weglowodany, bialko, kategoria, ilosc)
+
+            cursor.execute(query, values)
+
+            connection.commit()
+
+            print("Produkt został dodany do bazy danych.")
+        except Exception as error:
+            print("Błąd podczas dodawania produktu do bazy danych: ", error)
+
+    def odejmij_produkt(self, id_produktu, ilosc_do_odejscia):
+        try:
+            connection = self.db_connector.get_connection()
+            cursor = connection.cursor()
+
+            query = "SELECT ilosc FROM Produkty WHERE id = %s"
+            value = (id_produktu,)
+
+            cursor.execute(query, value)
+
+            current_quantity = cursor.fetchone()[0]
+
+            new_quantity = current_quantity - ilosc_do_odejscia
+
+            if new_quantity >= 0:
+                update_query = "UPDATE Produkty SET ilosc = %s WHERE id = %s"
+                update_values = (new_quantity, id_produktu)
+
+                cursor.execute(update_query, update_values)
+
+                connection.commit()
+
+                print("Ilość produktu została zaktualizowana.")
+            else:
+                print("Nie można odjąć większej ilości produktu niż aktualnie dostępnej")
+        except Exception as error:
+            print("Błąd podczas odejmowania produktu z bazy danych: ", error)
+
+
