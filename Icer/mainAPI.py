1a98898c25f6f95acf230e152153ebc457d65f28
s21165 <56755313+s21165@users.noreply.github.com>
2023-05-17 03:07:52 +0200

    Update mainAPI.py

--- Icer/mainAPI.py
+++ Icer/mainAPI.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from database_connector import DatabaseConnector
 from product_data import ProductData
-from value_manager import ProductManager
 
 app = Flask(__name__)
 
@@ -11,24 +10,17 @@ db_connector = DatabaseConnector("localhost", "root", "root", "Sklep")
 # Łączenie z bazą danych
 db_connector.connect()
 
-# Pobieranie połączenia z bazą danych
-connection = db_connector.get_connection()
-
 # Tworzenie instancji klasy ProductData
-product_data = ProductData(connection)
+product_data = ProductData(db_connector.get_connection())
 
-# Tworzenie instancji klasy ProductManager
-product_manager = ProductManager(db_connector)
-
-
-@app.route('/products', methods=['GET'])
+# Pobieranie danych produktów
+@app.route('/api/products', methods=['GET'])
 def get_products():
-    # Pobieranie danych produktów
     products = product_data.fetch_products()
     return jsonify(products)
 
-
-@app.route('/products', methods=['POST'])
+# Dodawanie produktu
+@app.route('/api/products', methods=['POST'])
 def add_product():
     data = request.get_json()
     nazwa = data['nazwa']
@@ -39,23 +31,27 @@ def add_product():
     bialko = data['bialko']
     kategoria = data['kategoria']
     ilosc = data['ilosc']
+
+    product_manager = value_manager.ProductManager(db_connector)
     product_manager.dodaj_produkt(nazwa, cena, kalorie, tluszcze, weglowodany, bialko, kategoria, ilosc)
-    return jsonify({'message': 'Produkt został dodany do bazy danych.'})
 
+    return jsonify({"message": "Produkt został dodany do bazy danych."})
 
-@app.route('/products/<product_name>', methods=['PUT'])
-def subtract_product(product_name):
+# Odejmowanie produktu
+@app.route('/api/products/<string:nazwa>', methods=['PUT'])
+def remove_product(nazwa):
     data = request.get_json()
-    ilosc_do_odejscia = data['ilosc_do_odejscia']
-    product_manager.odejmij_produkt(product_name, ilosc_do_odejscia)
-    return jsonify({'message': 'Ilość produktu została zaktualizowana.'})
+    ilosc_do_odejscia = data['ilosc']
 
+    product_manager = value_manager.ProductManager(db_connector)
+    product_manager.odejmij_produkt(nazwa, ilosc_do_odejscia)
+
+    return jsonify({"message": "Ilość produktu została zaktualizowana."})
 
 # Rozłączanie z bazą danych
 @app.teardown_appcontext
-def teardown_db(exception=None):
+def teardown_db(exception):
     db_connector.disconnect()
-
 
 if __name__ == '__main__':
     app.run()
