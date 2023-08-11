b2250c36375bb659bf49f28cc0b355daf74b23b2
s21488-pj <s21488@pjwstk.edu.pl>
2023-08-11 23:22:20 +0200

    zmiany wizualne

--- icer/src/Main.js
+++ icer/src/Main.js
@@ -34,7 +34,6 @@ function Main() {
     }, [isOpen]);
 
 
-
     const handleClick = () => {
 
         if (isOpen) {
@@ -88,49 +87,61 @@ function Main() {
                                     <Icon icon="material-symbols:arrow-forward-ios"/> :
                                     <Icon icon="material-symbols:arrow-back-ios-new"/>} </button>
                             </div>
-                            <div className="logoContainer">
-                                <img src={logo} alt="Your Image" className={isIcon ? "logo" : "smallLogo"}/>
-                            </div>
+                            <Link to="/">
+                                <div className="logoContainer">
+                                    <img src={logo} alt="Your Image" className={isIcon ? "logo" : "smallLogo"}/>
+                                </div>
+                            </Link>
 
                             {secoundMenu && (
                                 <div className={secoundMenu ? "visible" : "invisible"}>
 
                                     <div className="menuNav">
-                                        <div className="navDivMain">
-                                            <Link to="/Sklepy">{isIcon ? <h3>sklepy</h3> :
-                                                <Icon className="menuIcons" icon="mdi:shop-find"/>}</Link>
-                                        </div>
-                                        <div className="navDivMain">
-                                            <Link to="/Ustawienia">{isIcon ? <h3>ustawienia</h3> :
-                                                <Icon className="menuIcons"
-                                                      icon="fa6-solid:screwdriver-wrench"/>}</Link>
-                                        </div>
-                                        <div className="navDivMain">
-                                            <Link to="/Pomoc">{isIcon ? <h3>pomoc</h3> :
-                                                <Icon className="menuIcons" icon="solar:help-linear"/>}</Link>
-                                        </div>
-                                        <div className="navDivMain">
-                                            <Link to="/Produkty">{isIcon ? <h3>Produkty</h3> :
-                                                <Icon className="menuIcons"
-                                                      icon="streamline:shopping-bag-hand-bag-1-shopping-bag-purse-goods-item-products"/>}</Link>
-                                        </div>
-                                        <div className="navDivMain">
-                                            <Link to="/dodajProdukt">{isIcon ? <h3>Dodaj produkt</h3> :
-                                                <Icon className="menuIcons" icon="fluent:add-28-filled"/>}</Link>
-                                        </div>
+                                        <Link to="/Sklepy">
+                                            <div className="navDivMain">
+                                                {isIcon ? <h3>sklepy</h3> :
+                                                    <Icon className="menuIcons" icon="mdi:shop-find"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/Ustawienia">
+                                            <div className="navDivMain">
+                                                {isIcon ? <h3>ustawienia</h3> :
+                                                    <Icon className="menuIcons"
+                                                          icon="fa6-solid:screwdriver-wrench"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/Pomoc">
+                                            <div className="navDivMain">
+                                                {isIcon ? <h3>pomoc</h3> :
+                                                    <Icon className="menuIcons" icon="solar:help-linear"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/Produkty">
+                                            <div className="navDivMain">
+                                                {isIcon ? <h3>Produkty</h3> :
+                                                    <Icon className="menuIcons"
+                                                          icon="streamline:shopping-bag-hand-bag-1-shopping-bag-purse-goods-item-products"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/dodajProdukt">
+                                            <div className="navDivMain">
+                                                {isIcon ? <h3>Dodaj produkt</h3> :
+                                                    <Icon className="menuIcons" icon="fluent:add-28-filled"/>}
+                                            </div>
+                                        </Link>
 
-                                        <div className="navDivMain">
+                                        <button className="menuNavButton"
+                                                onClick={() => {
+                                                    setSecoundMenu(!secoundMenu)
+                                                }}
+                                        >
+                                            <div className="navDivMain">
 
-                                            <button className="menuNavButton"
-                                                    onClick={() => {
-                                                        setSecoundMenu(!secoundMenu)
-                                                    }}
-                                            >{isIcon ? <h3>menu</h3> :
-                                                <Icon className="menuIcons" icon="mdi:menu-up-outline"/>}
-                                            </button>
+                                                {isIcon ? <h3>menu</h3> :
+                                                    <Icon className="menuIcons" icon="mdi:menu-up-outline"/>}
+                                            </div>
+                                        </button>
 
-
-                                        </div>
 
                                         <div className="navDivMain">
                                             <Link to="/Wyloguj">{isIcon ? <h3>wyloguj</h3> :
@@ -143,37 +154,49 @@ function Main() {
                             <div className={secoundMenu ? "invisible" : "visible"}>
                                 <div className="menuNav">
                                     <div>
-                                        <div className="navDiv">
-                                            <Link to="/">{isIcon ? <h3>lodówka</h3> :
-                                                <Icon className="menuIcons" icon="tabler:fridge"/>}</Link>
-                                        </div>
-                                        <div className="navDiv">
-                                            <Link to="/HistoriaZakupow">{isIcon ? <h3>historia zakupów</h3> :
-                                                <Icon className="menuIcons"
-                                                      icon="material-symbols:history-rounded"/>}</Link>
-                                        </div>
-                                        <div className="navDiv">
-                                            <Link to="/Sklepy">{isIcon ? <h3>sklepy</h3> :
-                                                <Icon className="menuIcons" icon="mdi:shop-find"/>}</Link>
-                                        </div>
-                                        <div className="navDiv">
-                                            <Link to="/Konto">{isIcon ? <h3>konto</h3> : <Icon className="menuIcons"
-                                                                                               icon="material-symbols:account-circle-outline"/>}</Link>
-                                        </div>
-                                        <div className="navDiv">
-                                            <Link to="/Powiadomienia"> {isIcon ? <h3> powiadomienia</h3> :
-                                                <Icon className="menuIcons"
-                                                      icon="ic:baseline-notifications"/>}</Link>
-                                        </div>
-                                        <div className="navDiv">
-                                            <button className="menuNavButton"
-                                                    onClick={() => {
-                                                        setSecoundMenu(!secoundMenu)
-                                                    }}
-                                            >{isIcon ? <h3>menu</h3> :
-                                                <Icon className="menuIcons" icon="mdi:menu-down-outline"/>}
-                                            </button>
-                                        </div>
+                                        <Link to="/">
+                                            <div className="navDiv">
+                                                {isIcon ? <h3>lodówka</h3> :
+                                                    <Icon className="menuIcons" icon="tabler:fridge"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/HistoriaZakupow">
+                                            <div className="navDiv">
+                                                {isIcon ? <h3>historia zakupów</h3> :
+                                                    <Icon className="menuIcons"
+                                                          icon="material-symbols:history-rounded"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/Sklepy">
+                                            <div className="navDiv">
+                                                {isIcon ? <h3>sklepy</h3> :
+                                                    <Icon className="menuIcons" icon="mdi:shop-find"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/Konto">
+                                            <div className="navDiv">
+                                                {isIcon ? <h3>konto</h3> : <Icon className="menuIcons"
+                                                                                 icon="material-symbols:account-circle-outline"/>}
+                                            </div>
+                                        </Link>
+                                        <Link to="/Powiadomienia">
+                                            <div className="navDiv">
+                                                {isIcon ? <h3> powiadomienia</h3> :
+                                                    <Icon className="menuIcons"
+                                                          icon="ic:baseline-notifications"/>}
+                                            </div>
+                                        </Link>
+                                        <button className="menuNavButton"
+                                                onClick={() => {
+                                                    setSecoundMenu(!secoundMenu)
+                                                }}
+                                        >
+                                            <div className="navDiv">
+                                                {isIcon ? <h3>menu</h3> :
+                                                    <Icon className="menuIcons" icon="mdi:menu-down-outline"/>}
+
+                                            </div>
+                                        </button>
 
                                     </div>
 
