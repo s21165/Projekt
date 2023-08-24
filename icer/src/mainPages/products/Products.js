9859fc326f815d356a7ce43655fd355e90280d79
s21488-pj <s21488@pjwstk.edu.pl>
2023-08-24 14:21:34 +0200

    edycjaPorduktuZrobiona

--- icer/src/mainPages/products/Products.js
+++ icer/src/mainPages/products/Products.js
@@ -6,18 +6,63 @@ function Products() {
     const [data, setData] = useState(null);
     // let [jsonData, setJsonData] = useState(null);
     const [refresh, setRefresh] = useState(false); // Dodajemy stan do odświeżania ekranu
+    const [editingProduct, setEditingProduct] = useState({
+        nazwa: '',
+        cena: '',
+        kalorie: '',
+        tluszcze: '',
+        weglowodany: '',
+        bialko: '',
+        kategoria: '',
+        ilosc: '',
+        data_waznosci:'',
+    });
+    const handleEditClick = (product) => {
+        setEditingProduct({
+            id: product[0],
+            nazwa: product[1],
+            cena: product[2],
+            kalorie: product[3],
+            tluszcze: product[4],
+            weglowodany: product[5],
+            bialko: product[6],
+            kategoria: product[7],
+            ilosc: product[8],
+            data_waznosci: product[9]
+        });
+    };
+    const handleEdit =() => {
+        const id = editingProduct.id;
+        const config = {
+            headers: {
+                'Content-Type': 'application/json', // informuje serwer, że dane wysyłane w żądaniu są w formacie JSON.
+            },
+        };
 
+        axios
+            .put(`http://localhost:5000/api/edit_product/${id}`, editingProduct, config)
+            .then((response) => {
+                console.log(response.data);
+                setEditingProduct(null);
+                setRefresh(!refresh);
+            })
+            .catch((error) => {
+                console.error(`There was an error updating the product: ${error}`);
+            });
+    };
 
     useEffect(() => {
+        setEditingProduct(null);
         axios.get('http://localhost:5000/api/products')  // zmień URL na rzeczywisty adres endpointa
             .then((response) => {
                 setData(response.data);
-                console.log("sama data:" + data);
+                // console.log("sama data:" + data);
+
             })
             .catch((error) => {
                 console.error(`There was an error retrieving the data: ${error}`);
             });
-    }, []);
+    }, [refresh]);
     // useEffect(() => {
     //     if (data) {
     //         setJsonData = JSON.stringify(data);  // Przekształcenie obiektu JavaScript na dane w formacie JSON
@@ -27,15 +72,15 @@ function Products() {
     const handleRemove = (id) => {
         const config = {
             headers: {
-                'Content-Type': 'application/json', // Ustawienie nagłówka Content-Type na application/json
+                'Content-Type': 'application/json',
             },
         };
+
         axios
-            .put(`http://localhost:5000/api/products/${id}`, {}, config)
+            .delete(`http://localhost:5000/api/products/${id}`, config)
             .then((response) => {
                 console.log(response.data);
-                // Możesz wykonać odpowiednie akcje po usunięciu produktu
-                setRefresh(!refresh); // Zmieniamy stan "refresh", aby odświeżyć ekran
+                setRefresh(!refresh); // Refresh the product list after deletion
             })
             .catch((error) => {
                 console.error(`There was an error removing the product: ${error}`);
@@ -45,7 +90,87 @@ function Products() {
     return (
         <>
             <div className="productList">
-                {data && data.map((data, index) =>
+                {editingProduct && (
+                    <div className="productItem">
+
+                        <p>
+                            Nazwa:
+                            <input
+                                type="text"
+                                value={editingProduct.nazwa}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, nazwa: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Cena:
+                            <input
+                                type="text"
+                                value={editingProduct.cena}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, cena: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Kalorie:
+                            <input
+                                type="text"
+                                value={editingProduct.kalorie}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, kalorie: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Tłuszcze:
+                            <input
+                                type="text"
+                                value={editingProduct.tluszcze}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, tluszcze: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Węglowodany:
+                            <input
+                                type="text"
+                                value={editingProduct.weglowodany}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, weglowodany: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Białko:
+                            <input
+                                type="text"
+                                value={editingProduct.bialko}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, bialko: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Kategoria:
+                            <input
+                                type="text"
+                                value={editingProduct.kategoria}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, kategoria: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Ilość:
+                            <input
+                                type="text"
+                                value={editingProduct.ilosc}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, ilosc: e.target.value }))}
+                            />
+                        </p>
+                        <p>
+                            Data ważności:
+                            <input
+                                type="text"
+                                value={editingProduct.data_waznosci}
+                                onChange={e => setEditingProduct(prev => ({ ...prev, data_waznosci: e.target.value }))}
+                            />
+                        </p>
+
+                        <button onClick={() => handleEdit()}>Zaktualizuj</button>
+                        <button onClick={() => setEditingProduct(null)}>Anuluj</button>
+
+                    </div>
+                )}{data && !editingProduct &&data.map((data, index) =>
                     <div key={index} className="productItem">
                         <p><h2>Nazwa: {data[1]}</h2></p>
                         <p><h3>Cena: {data[2]}</h3></p>
@@ -57,6 +182,9 @@ function Products() {
                         <p><h3>Ilość: {data[8]}</h3></p>
                         <button onClick={() => handleRemove(data[0])} data-id={data[0]}>
                             <h2>USUŃ</h2>
+                        </button>
+                        <button onClick={() => handleEditClick(data)} data-id={data[0]}>
+                            <h2>EDYTUJ</h2>
                         </button>
                     </div>
                 )}
