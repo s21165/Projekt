b763377355b4b9a75077ad5983bcacf8133c7d97
s21165 <56755313+s21165@users.noreply.github.com>
2023-08-26 16:18:18 +0200

    don't open, death inside

--- Icer/mainAPI.py
+++ Icer/mainAPI.py
@@ -1,70 +1,218 @@
+# Zaimportuj wszystkie potrzebne moduły
+import uuid
+import bcrypt
+import requests
+import logging
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify, session, redirect
 from flask_cors import CORS
-import requests
-from dotenv import load_dotenv
-import bcrypt
-
-from modules import value_manager
+from jsonschema._validators import const
 from node_modules.database_connector import DatabaseConnector
 from modules.product_data import ProductData
-from datetime import datetime, timedelta
-
 from modules.value_manager import ProductManager
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_talisman import Talisman
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+from flask import Flask, request, jsonify, session, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+USE_TEST_SESSION = True  # Jeśli ustawione na True, korzystaj z sesji testowej
+TEST_SESSION = {
+    'username': 'test_user',
+    'session_id': 'test_session_id'
+}
+
 
 app = Flask(__name__)
-CORS(app)
-app.secret_key = 'secret_key'  # Klucz sesji
-
-# Ładuj zmienne środowiskowe z pliku .env
-load_dotenv()
-
-# Tworzenie instancji klasy DatabaseConnector
+# Tworzenie instancji klasy DatabaseConnector i łączenie z bazą danych
 db_connector = DatabaseConnector("localhost", "root", "root", "Sklep")
-
-# Łączenie z bazą danych
 db_connector.connect()
 
 # Tworzenie instancji ProductManager
 product_manager = ProductManager(db_connector)
+# Inicjalizacja aplikacji Flask
+
+
+
+
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+# Konfiguracja klucza sesji
+app.secret_key = uuid.uuid4().hex
+
+# Konfiguracja Flask-Session
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_KEY_PREFIX'] = 'shopping_app_'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+# Konfiguracja bazy danych SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost:5432/mydatabase'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Konfiguracja logowania
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Konfiguracja Flask-Talisman dla HTTPS
+talisman_config = {
+    'session_cookie_secure': False,  # Dla celów deweloperskich. Ustaw na True w środowisku produkcyjnym
+}
+talisman = Talisman(app, **talisman_config)
+
+
+
+app.config['SECRET_KEY'] = 'moj_tajny_klucz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data['username'], password=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully!"}), 201
+
+## funkcje wykorzystywane do rejestracji
+def user_exists(username):
+    query = "SELECT * FROM Users WHERE username = %s"
+    values = (username,)
+    cursor = db_connector.get_connection().cursor()
+    cursor.execute(query, values)
+    result = cursor.fetchone()
+    cursor.close()
+
+    return True if result else False
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        # Jeśli korzystamy z sesji testowej
+        if USE_TEST_SESSION:
+            session['username'] = TEST_SESSION['username']
+            session['session_id'] = TEST_SESSION['session_id']
+            return jsonify({"message": "Login successful (test session)", "session_id": TEST_SESSION['session_id']})
+
+        username = data.get('username')
+        password = data.get('password')
+
+        if not username or not password:
+            return jsonify({"message": "Username or password not provided"}), 400
+
+        def check_user(username, password):
+            try:
+                # Pobierz użytkownika o podanej nazwie użytkownika
+                user = User.query.filter_by(username=username).first()
+                if not user:
+                    return False
+
+                # Porównaj podane hasło z hasłem w bazie danych
+                if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                    return True
+                else:
+                    return False
+            except Exception as e:
+                logger.error(f"Error checking user: {str(e)}")
+                return False
+
+
+def check_user(username, password):
+    try:
+        # Pobierz użytkownika o podanej nazwie użytkownika
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return False
+
+        # Porównaj podane hasło z hasłem w bazie danych
+        if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            return True
+        else:
+            return False
+    except Exception as e:
+        logger.error(f"Error checking user: {str(e)}")
+        return False
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(405)
+def handle_405(error):
+    return jsonify({"error": "Method not allowed"}), 405
+
+
+@app.errorhandler(500)
+def handle_500(error):
+    logger.error(f"Internal error: {str(error)}")
+    return jsonify({"error": "Internal Server Error"}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+def is_user_logged_in(session_id):
+    return 'username' in session and session.get('session_id') == session_id
 
 
 @app.route('/api/add_product', methods=['POST'])
 def add_product():
-    # Tworzenie instancji klasy DatabaseConnector
+    print("dziala?")
+    print("Session data:", session)
+
+    # Pobranie sessionId z nagłówków żądania
+    session_id = request.headers.get('session_id')
+
+    if not is_user_logged_in(session_id):
+        return jsonify({"error": "User not logged in or invalid session ID"}), 401
+
     db_connector = DatabaseConnector("localhost", "root", "root", "Sklep")
-
-    # Łączenie z bazą danych
     db_connector.connect()
-
-    # Tworzenie instancji ProductManager
     product_manager = ProductManager(db_connector)
 
     connection = None
     cursor = None
-
     try:
-
-        # Sprawdzenie, czy użytkownik jest zalogowany
-        if 'username' not in session:
-            return jsonify({"error": "User not logged in"}), 401
-
         connection = db_connector.get_connection()
         cursor = connection.cursor(dictionary=True)
 
         username = session['username']
 
-        # Pobranie ID użytkownika na podstawie nazwy użytkownika
         user_query = "SELECT id FROM Users WHERE username = %s"
         cursor.execute(user_query, (username,))
         user_result = cursor.fetchone()
 
         if not user_result:
-            return jsonify({"error": "User not found"}), 401
+            return jsonify({"error": "User not found"}), 404
 
         data = request.json
         user_id = user_result['id']
 
-        # Sprawdzenie, czy produkt już istnieje
         check_product_query = "SELECT id FROM Produkty WHERE nazwa = %s AND cena = %s"
         cursor.execute(check_product_query, (data['nazwa'], data['cena']))
         product_exists = cursor.fetchone()
@@ -73,28 +221,23 @@ def add_product():
             product_id = product_exists['id']
         else:
             product_id = product_manager.dodaj_produkt(
-                data['nazwa'],
-                data['cena'],
-                data['kalorie'],
-                data['tluszcze'],
-                data['weglowodany'],
-                data['bialko'],
-                data['kategoria']
+                data['nazwa'], data['cena'], data['kalorie'],
+                data['tluszcze'], data['weglowodany'],
+                data['bialko'], data['kategoria']
             )
 
             if product_id is None:
-                return jsonify({"error": "Failed to add product to Produkty table."})
+                return jsonify({"error": "Failed to add product to Produkty table."}), 500
 
-        # Dodawanie produktu do tabeli Icer
         icer_query = "INSERT INTO Icer (UserID, produktID, ilosc, data_waznosci) VALUES (%s, %s, %s, %s)"
         cursor.execute(icer_query, (user_id, product_id, data['ilosc'], data['data_waznosci']))
-
         connection.commit()
 
         return jsonify({"message": "Produkt został dodany!"})
 
     except Exception as error:
         return jsonify({"error": str(error)}), 500
+
     finally:
         if cursor:
             cursor.close()
@@ -135,12 +278,18 @@ def edit_product(product_id):
 
 @app.route('/api/Icer', methods=['GET'])
 def get_icer():
+    # Najpierw sprawdzamy session_id w nagłówkach
+    session_id_header = request.headers.get('session_id')
+
+    if 'sessionId' not in session or session['sessionId'] != session_id_header:
+        return jsonify({"error": "Invalid session ID"}), 401
+
     connection = db_connector.get_connection()
     cursor = connection.cursor(dictionary=True)
 
     try:
         if 'username' not in session:
-            return jsonify({"error": "User not logged in"}), 401
+            return jsonify({"error": "User not logged in"}), 419
 
         # Pobranie ID aktualnie zalogowanego użytkownika
         username = session['username']
@@ -151,7 +300,7 @@ def get_icer():
         if not user_result:
             return jsonify({"error": "User not found"}), 401
 
-        # Modyfikacja zapytania SQL, aby pokazywać wszystkie informacje o produkcie
+        # Modyfikacja zapytania SQL
         user_id = user_result['id']
         query = """
             SELECT Icer.id, Icer.UserID, Icer.produktID, Icer.ilosc, 
@@ -230,99 +379,6 @@ def get_products():
     return jsonify(products)
 
 
-from flask import session, jsonify, request
-import uuid  # potrzebne do generowania unikalnych ID sesji
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        data = request.get_json()
-        username = data['username']
-        password = data['password']
-
-        # Sprawdzanie, czy użytkownik istnieje w bazie danych
-        if check_user(username, password):
-            # Utworzenie sesji dla zalogowanego użytkownika
-            session['username'] = username
-            session_id = str(uuid.uuid4())  # Generowanie unikalnego ID sesji
-            session['session_id'] = session_id  # Przechowywanie ID sesji
-            return jsonify({"message": "Login successful", "session_id": session_id})
-        else:
-            return jsonify({"message": "Invalid credentials"}), 401
-    else:
-        return jsonify({"message": "Method not allowed"}), 405
-
-
-## funkcje wykorzystywane do rejestracji
-def user_exists(username):
-    query = "SELECT * FROM Users WHERE username = %s"
-    values = (username,)
-    cursor = db_connector.get_connection().cursor()
-    cursor.execute(query, values)
-    result = cursor.fetchone()
-    cursor.close()
-
-    return True if result else False
-
-
-def save_user(username, hashed_pw):
-    try:
-        query = "INSERT INTO Users (username, password) VALUES (%s, %s)"
-        values = (username, hashed_pw.decode('utf-8'))
-        cursor = db_connector.get_connection().cursor()
-        cursor.execute(query, values)
-        db_connector.get_connection().commit()
-        return True
-    except Exception as error:
-        print("Error during registration:", error)
-        return False
-    finally:
-        cursor.close()
-
-
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-
-    # Sprawdzenie, czy użytkownik już istnieje
-    if user_exists(username):
-        return jsonify({"message": "User already exists"}), 400
-
-    # Szyfrowanie hasła
-    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-    # Zapisanie użytkownika w bazie danych
-    if save_user(username, hashed_pw):
-        return jsonify({"message": "Registration successful"})
-    else:
-        return jsonify({"message": "Registration failed"}), 500
-
-
-def check_user(username, password):
-    try:
-        query = "SELECT password FROM Users WHERE username = %s"
-        values = (username,)
-        cursor = db_connector.get_connection().cursor()
-        cursor.execute(query, values)
-        result = cursor.fetchone()
-
-        if result:
-            db_password = result[0].encode('utf-8')
-            if bcrypt.checkpw(password.encode('utf-8'), db_password):
-                session['username'] = username
-                return True
-        return False
-
-    except Exception as error:
-        print("Error during user authentication:", error)
-        return False
-    finally:
-        cursor.close()
-
-
 @app.route('/api/edit_user', methods=['POST'])
 def edit_user():
     print("test")
@@ -367,13 +423,11 @@ def edit_user():
         return jsonify({"error": str(error)})
 
 
-# Strona wylogowania
-@app.route('/logout')
+@app.route('/logout', methods=['GET'])
 def logout():
-    # Usunięcie sesji
     session.pop('username', None)
     return redirect('/login')
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
