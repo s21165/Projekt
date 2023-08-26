371cf4712f83eb2bf42d6ff1220b091435b5b1b8
s21488-pj <s21488@pjwstk.edu.pl>
2023-08-26 17:32:11 +0200

    zmiany

--- icer/src/mainPages/products/Products.js
+++ icer/src/mainPages/products/Products.js
@@ -5,6 +5,7 @@ import ProductEdit from "./ProductEdit";
 import ProductItem from "./ProductItem";
 import { useContext } from 'react';
 import { AuthContext } from '../account/auth-context';
+import {json} from "react-router-dom";
 
 function Products() {
 
@@ -12,141 +13,71 @@ function Products() {
     const sessionId = user ? user.sessionId : null;
     const [data, setData] = useState(null);
 
-    // let [jsonData, setJsonData] = useState(null);
 
     const [refresh, setRefresh] = useState(false); // Dodajemy stan do odświeżania ekranu
     const [editingProduct, setEditingProduct] = useState(null);
-    const handleEditClick = (product) => {
-        setEditingProduct({
-            id: product[0],
-            nazwa: product[1],
-            cena: product[2],
-            kalorie: product[3],
-            tluszcze: product[4],
-            weglowodany: product[5],
-            bialko: product[6],
-            kategoria: product[7],
-            ilosc: product[8],
-            data_waznosci: new Date(product[9]).toISOString().split('T')[0]
-        });
-    };
-    const handleEdit = () => {
-        const id = editingProduct.id;
-        const config = {
-            headers: {
-                'Content-Type': 'application/json', // informuje serwer, że dane wysyłane w żądaniu są w formacie JSON.
-                'session_id': sessionId
-
-            },
-        };
-
-        axios
-            .put(`http://localhost:5000/api/edit_product/${id}`, editingProduct, config)
-            .then((response) => {
-                console.log(response.data);
-                setEditingProduct(null);
-                setRefresh(!refresh);
-            })
-            .catch((error) => {
-                console.error(`There was an error updating the product: ${error}`);
-            });
-    };
-    const handleIncrease = (productId) => {
-        // Znajdź produkt o danym ID i zwiększ jego ilość
-        const updatedProducts = data.map(product => {
-            if (product[0] === productId) {
-                product[8]++; // Zwiększenie ilości o 1
-            }
-            return product;
-        });
-        setData(updatedProducts);
-    }
-
-    const handleDecrease = (productId) => {
-        // Znajdź produkt o danym ID i zmniejsz jego ilość
-        const updatedProducts = data.map(product => {
-            if (product[0] === productId && product[8] > 0) {
-                product[8]--; // Zmniejszenie ilości o 1
-            }
-            return product;
-        });
-        setData(updatedProducts);
-    }
 
 
     useEffect(() => {
+
         setEditingProduct(null);
-        axios.get('http://localhost:5000/api/Icer', { withCredentials: true })
+        console.log(sessionId)
+
+        axios.post('http://localhost:5000/api/Icer',  { sessionId: sessionId })
             .then((response) => {
-                setData(response.data);
-
-
-                 console.log("sama data:" + data);
+                if (Array.isArray(response.data)) {
+                    setData(response.data);
+                } else {
+                    console.warn("The response data is not an array!");
+                }
+                console.log("sama data:" + JSON.stringify(response.data[0]));
 
             })
             .catch((error) => {
                 console.error(`There was an error retrieving the data: ${error}`);
             });
     }, [refresh]);
-    // useEffect(() => {
-    //     if (data) {
-    //         setJsonData = JSON.stringify(data);  // Przekształcenie obiektu JavaScript na dane w formacie JSON
-    //         console.log("jsondata: " + jsonData);
-    //     }
-    // }, [refresh]);
-    const handleRemove = (id) => {
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Session-ID': sessionId
-            },
-        };
-
-        axios
-            .delete(`http://localhost:5000/api/products/${id}`, config)
-            .then((response) => {
-                console.log(response.data);
-                setRefresh(!refresh); // Refresh the product list after deletion
-            })
-            .catch((error) => {
-                console.error(`There was an error removing the product: ${error}`);
-            });
-    };
 
     return (
         <>
             <div className="listButtons">
                 <div className="leftButtonDiv">
-                    <button className="leftButton" onClick={() => handleRemove(data[0])}>Aktualne</button>
+                    <button className="leftButton" >Aktualne</button>
                 </div>
                 <div className="rightButtonDiv">
-                    <button className="rightButton" onClick={() => handleIncrease(data[0])}>Stare</button>
+                    <button className="rightButton" >Stare</button>
                 </div>
             </div>
-            <div className="productList">
+            <div className="siema">
+                {data && data.map((data, index) =>
+    <>
+                    <p key={index}>(data[1]): {data.ilosc}</p>
+                    <p> key={index}>(data[1]): {data.nazwa}</p>
+    </>)
+                }
+                {/*{editingProduct && (*/}
+                {/*    <ProductEdit*/}
+                {/*        product={editingProduct}*/}
+                {/*        // handleEdit={handleEdit}*/}
+                {/*        setEditingProduct={setEditingProduct}*/}
+                {/*    />*/}
+                {/*)}*/}
+            {/*    {data && !editingProduct && data.map((data, index) =>*/}
 
-                {editingProduct && (
-                    <ProductEdit
-                        product={editingProduct}
-                        handleEdit={handleEdit}
-                        setEditingProduct={setEditingProduct}
-                    />
-                )}{data && !editingProduct && data.map((data, index) =>
-
-                <ProductItem
-                    key={index}
-                    data={data}
-                    handleRemove={handleRemove}
-                    handleEditClick={handleEditClick}
-                    handleIncrease={handleIncrease}
-                    handleDecrease={handleDecrease}
-                />
-            )}
+            {/*    // <ProductItem*/}
+            {/*    //     key={index}*/}
+            {/*    //     data={data}*/}
+            {/*    //     // handleRemove={handleRemove}*/}
+            {/*    //     // handleEditClick={handleEditClick}*/}
+            {/*    //     // handleIncrease={handleIncrease}*/}
+            {/*    //     // handleDecrease={handleDecrease}*/}
+            {/*    // />*/}
+            {/*)}*/}
             </div>
             <div>
 
             </div>
         </>
     );
-};
+}
 export default Products;
