d8435831b4015dbacad849fc584ea5d397c25e6f
Okti18861 <136648498+Okti18861@users.noreply.github.com>
2024-01-16 23:05:14 +0100

    v 0.578a
    -Removed barcode for adi

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from flask import Flask, request, render_template, url_for
 from modules.foodIdent_module.foodIdent import load_and_prep_image, pred_and_plot, load_model
 
 from modules.bot_module.bot import get_bot_response
-from modules.scan_module.gen import generate_qr_code, generate_barcode
-from modules.scan_module.forms import BarcodeForm
-from modules.scan_module.decoder import decode_barcode, decode_qr_code
+from modules.scan_module.gen import generate_qr_code # ,generate_barcode
+#from modules.scan_module.forms import BarcodeForm
+from modules.scan_module.decoder import decode_qr_code # ,decode_barcode
 from modules.advert_module.monitor import generate_frames
 
 import sys
@@ -31,11 +31,11 @@ class_names = ['ciasto_czekoladowe', 'donut', 'frytki', 'hamburger', 'lasagne', 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     bot_response = ""
-    barcode_form = BarcodeForm()  # For barcode form
+    #barcode_form = BarcodeForm()  # For barcode form
     if request.method == 'POST':
         user_input = request.form['user_input']
         bot_response = get_bot_response(user_input)
-    response = make_response(render_template('index.html', bot_response=bot_response, form=barcode_form))
+    response = make_response(render_template('index.html', bot_response=bot_response, ))
     response.headers['Content-Type'] = 'text/html; charset=utf-8'
     return response
 
@@ -70,38 +70,38 @@ def generate_qr_code_route():
     qr_code_image_path = os.path.join(app.config['QR_CODE_FOLDER'], qr_code_image_filename)
     return send_file(qr_code_image_path, mimetype='image/png')
 
-@app.route('/generate_barcode', methods=['POST'])
-def generate_barcode_route():
-    if request.method == 'POST':
-        data = {
-            "name": request.form['name'],
-            "price": request.form['price'],
-            "kcal": request.form['kcal'],
-            "fat": request.form['fat'],
-            "carbs": request.form['carbs'],
-            "protein": request.form['protein'],
-            "category": request.form['category'],
-            "amount": request.form['amount']
-        }
-        barcode_image_filename = generate_barcode(data, app.config['BARCODE_FOLDER'])
-        barcode_image_path = os.path.join(app.config['BARCODE_FOLDER'], barcode_image_filename)
-        return send_file(barcode_image_path, mimetype='image/png', as_attachment=True)
+# @app.route('/generate_barcode', methods=['POST'])
+# def generate_barcode_route():
+    # if request.method == 'POST':
+        # data = {
+            # "name": request.form['name'],
+            # "price": request.form['price'],
+            # "kcal": request.form['kcal'],
+            # "fat": request.form['fat'],
+            # "carbs": request.form['carbs'],
+            # "protein": request.form['protein'],
+            # "category": request.form['category'],
+            # "amount": request.form['amount']
+        # }
+        # barcode_image_filename = generate_barcode(data, app.config['BARCODE_FOLDER'])
+        # barcode_image_path = os.path.join(app.config['BARCODE_FOLDER'], barcode_image_filename)
+        # return send_file(barcode_image_path, mimetype='image/png', as_attachment=True)
 
-@app.route('/decode_barcode', methods=['POST'])
-def decode_barcode_route():
-    if 'barcode_image' not in request.files:
-        return "No barcode image uploaded"
+# @app.route('/decode_barcode', methods=['POST'])
+# def decode_barcode_route():
+    # if 'barcode_image' not in request.files:
+        # return "No barcode image uploaded"
     
-    barcode_image = request.files['barcode_image']
-    if barcode_image.filename == '':
-        return "No selected file"
+    # barcode_image = request.files['barcode_image']
+    # if barcode_image.filename == '':
+        # return "No selected file"
     
-    barcode_image_path = os.path.join(app.config['BARCODE_FOLDER'], barcode_image.filename)
-    barcode_image.save(barcode_image_path)
+    # barcode_image_path = os.path.join(app.config['BARCODE_FOLDER'], barcode_image.filename)
+    # barcode_image.save(barcode_image_path)
     
-    decoded_data = decode_barcode(barcode_image_path)
+    # decoded_data = decode_barcode(barcode_image_path)
     
-    return f"Decoded Barcode Data: {decoded_data}"
+    # return f"Decoded Barcode Data: {decoded_data}"
 
 @app.route('/decode_qr_code', methods=['POST'])
 def decode_qr_code_route():
