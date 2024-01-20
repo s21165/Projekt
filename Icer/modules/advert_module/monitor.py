8867bc0089a50d471c8927f3e1078bd1874c3496
s21488-pj <s21488@pjwstk.edu.pl>
2024-01-20 16:29:57 +0100

    changes, work now

--- Icer/modules/advert_module/monitor.py
+++ Icer/modules/advert_module/monitor.py
@@ -6,6 +6,16 @@ import winsound
 import time
 
 
+def print_and_send(data):
+    print(data)
+    api_endpoint = "http://192.168.0.130:5000/start_camera_monitoring"
+    try:
+        response = requests.post(api_endpoint, json={'dane': data})
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Błąd wysyłania danych: {e}")
+
+
 # Function to detect faces and eyes in an image
 def detect_faces_and_eyes(image):
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
