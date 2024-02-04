9db4ff2a3004d7a12249f506a59399d10f249981
Okti18861 <136648498+Okti18861@users.noreply.github.com>
2024-02-04 16:34:05 +0100

    Update decoder.py

--- Icer/modules/scan_module/decoder.py
+++ Icer/modules/scan_module/decoder.py
@@ -1,20 +1,9 @@
-# from barcode import get_barcode
 import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
 import json
 import os
 
-# Function to decode a barcode from an image
-# def decode_barcode(image_path):
-    # barcode_image = Image.open(image_path)
-    # decoded_objects = decode(barcode_image)
-    
-    # if decoded_objects:
-        # decoded_data = decoded_objects[0].data.decode('utf-8')
-        # return decoded_data
-    # else:
-        # return "No barcode found"
 
 # Dekodowanie QR z obrazu
 def decode_qr_code(image_path):
