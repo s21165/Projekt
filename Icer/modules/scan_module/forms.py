e0b442ec7846ca264f56e17358a6696b149080a6
Okti18861 <136648498+Okti18861@users.noreply.github.com>
2024-02-05 09:39:01 +0100

    Delete Icer/modules/scan_module/forms.py

--- Icer/modules/scan_module/forms.py
+++
@@ -1,9 +0,0 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField
-
-# Form class for generating barcode
-# class BarcodeForm(FlaskForm):
-    # manufacturer = StringField('Manufacturer')  
-    # date = StringField('Date')  
-    # product_id = StringField('Product ID')  
-    # submit = SubmitField('Generate Barcode') 
