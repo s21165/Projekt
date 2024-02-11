80e0c473bc23302cd9e16f1df8052cb022c1acf9
s21165 <56755313+s21165@users.noreply.github.com>
2024-02-11 20:12:14 +0100

    Update image_handler.py
    missing files deleted in quick fix

--- Icer/modules/image_handler.py
+++ Icer/modules/image_handler.py
@@ -98,3 +98,23 @@ def change_user_profile(db_connector, user_id, image_data_base64):
 
             elif profile_result[0] == 1:
                 # Update lokalizacji zdjęcia użytkownika
+                update_query = "UPDATE preferencje_uzytkownikow SET lokalizacja_zdj = %s, podstawowe_profilowe=%s WHERE UserID = %s"
+                cursor.execute(update_query, (image_name, 0, user_id))
+        else:
+            # Brak wpisu dla tego użytkownika, wstaw nowy wpis
+            insert_query = """
+                INSERT INTO preferencje_uzytkownikow 
+                (UserID, podstawowe_profilowe, lokalizacja_zdj)
+                VALUES (%s, %s, %s)
+            """
+            cursor.execute(insert_query, (user_id, 0, image_name))
+
+        connection.commit()
+        cursor.close()
+
+        return jsonify({"message": "User profile image changed successfully"})
+
+    except ValueError as ve:
+        return jsonify({"error": f"Error: {str(ve)}"}), 400
+    except Exception as error:
+        return jsonify({"error": f"Unexpected error: {str(error)}"}), 500
